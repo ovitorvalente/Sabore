02fbb6f3d40cd8f9a5273eebfb16dc1c86520770
Vitor Valente <ovitorvalente@gmail.com>
2025-05-06 23:07:33 -0300

    feat: adiciona o controle para pegr os produtos e para cadastra-los

---
+++ Sabore.API/Controllers/ProdutoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sabore.BLL;
+using Sabore.Models;
+
+namespace Sabore.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly ProdutoService _produtoService;
+
+        public ProdutoController()
+        {
+            _produtoService = new ProdutoService();
+        }
+
+        [HttpGet]
+        public IActionResult GetProdutos()
+        {
+            var listaProdutos = _produtoService.ListarProdutos();
+            return Ok(listaProdutos);
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarProduto([FromBody] Produto param)
+        {
+            var resut = _produtoService.CadastrarProduto(param);
+            if (!resut)
+                return BadRequest("Erro ao cadastrar produto");
+
+            return Ok("Produto cadastrado com sucesso!");
+        }
+    }
+}
